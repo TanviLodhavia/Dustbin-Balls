9b8970b66af2654662af60a9b8da787ae20a0322
TanviLodhavia <65407346+TanviLodhavia@users.noreply.github.com>
2021-04-13 19:45:04 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -3,10 +3,12 @@ const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 const Render = Matter.Render;
+const Constraint = Matter.Constraint;
 var paperBall;
 var ground;
 var base,left,right;
 var img;
+var launcher;
 
 function preload(){
 	img = loadImage("dustbingreen.png");
@@ -24,6 +26,7 @@ function setup() {
 	left = new Dustbin (905,310,13,100);
 	right = new Dustbin (1095,310,13,100);
 	base = new Dustbin (1000,350,200,20);
+	launcher = new Launcher(paperBall.body, {x:150, y:330})
 	sprite = createSprite(1000,245,20,20);
 	sprite.addImage(img);
 	sprite.scale = 0.8;
@@ -39,6 +42,7 @@ function draw() {
   base.display();
   left.display();
   right.display();
+  launcher.display();
   
   drawSprites(); 
   //paperBall.velocityY = paperBall.velocityY + 0.8
